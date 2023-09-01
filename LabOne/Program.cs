0a70f1c354be8b4304be9535808f5a06aaa2a45d
brysbran <brandonbc@etsu.edu>
2023-09-01 14:55:00 -0400

    first

---
+++ LabOne/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace LabOne
+{
+    using System.IO;
+    using System.Collections.Generic;
+    using System;
+    using System.Linq;
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<VideoGame> videogame = File.ReadAllLines("F:\\ETSU_Fall_23\\Server_Side\\Labs\\LabOne\\LabOne\\videogames.csv").Skip(1).Select(v => VideoGame.FromFile(v)).ToList();
+
+            //the Sort(); method checks if VideoGame implements IComparable and internally calls the CompareTo method
+            videogame.Sort();
+            Console.WriteLine("Titles in Alphabetical Order:");
+            for (int i = 121; i < 140; i++)
+            {
+                {
+                    Console.WriteLine(videogame[i].name);
+                }
+            }
+            Console.WriteLine("\n----------------------------------------------------------------------------------------------------------");
+            //isolate and print ubisoft games, part 3
+            Console.WriteLine("Ubisoft Games: ");
+            var ubisoftGames = videogame.Where(videogame => videogame.publisher.Contains("Ubisoft")).Select(videogame => videogame.name).ToList();
+            ubisoftGames.Sort();
+            foreach(var name in  ubisoftGames)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("\n------------------------------------------------------------------------------------------------------------");
+            //part 4
+            VideoGame.PublisherStatistics(videogame, "Ubisoft");
+            Console.WriteLine("\n-------------------------------------------------------------------------------------------------------------");
+            //part 5
+            Console.WriteLine("List of Adventure Games: ");
+            var adventureGames = videogame.Where(videogame => videogame.genre.Contains("Adventure")).Select(videogame => videogame.name).ToList();
+            adventureGames.Sort();
+            foreach(var name in adventureGames)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("\n-------------------------------------------------------------------------------------------------------------");
+            //part 6
+            Console.WriteLine("ADVENTURE GAME STATS: ");
+            VideoGame.GenreStatistics(videogame, "Adventure");
+
+            Console.WriteLine("\n--------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Hello! Please type the name of a video game publisher you would like statistics on!");
+            string publisher = Console.ReadLine();
+            VideoGame.PublisherData(videogame, publisher);
+            VideoGame.PublisherStatistics(videogame, publisher);
+
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Hello! Please type the name of a video game genre you would like statistics on!");
+            string genre = Console.ReadLine();
+            VideoGame.GenreData(videogame, genre);
+            VideoGame.GenreStatistics(videogame, genre);
+        }
+    }
+}
---
+++ LabOne/VideoGame.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabOne
+{
+    public class VideoGame : IComparable<VideoGame>
+    {
+
+        public string name;
+        public string platform;
+        public int year;
+        public string genre;
+        public string publisher;
+        public string NA_sales;
+        public string EU_sales;
+        public string JP_sales;
+        public string other_sales;
+        public string global_sales;
+
+
+
+        public static VideoGame FromFile(string line)
+        {
+
+            //read first line bc it doesnt have real info
+            //read values to array
+            string[] info = line.Split(',');
+            VideoGame reader = new VideoGame();
+            //set values in array
+            reader.name = Convert.ToString(info[0]);
+            reader.platform = Convert.ToString(info[1]);
+            reader.year = Convert.ToInt32(info[2]);
+            reader.genre = Convert.ToString(info[3]);
+            reader.publisher = Convert.ToString(info[4]);
+            reader.NA_sales = Convert.ToString(info[5]);
+            reader.EU_sales = Convert.ToString(info[6]);
+            reader.JP_sales = Convert.ToString(info[7]);
+            reader.other_sales = Convert.ToString(info[8]);
+            reader.global_sales = Convert.ToString(info[9]);
+            return reader;
+
+        }
+
+        public static string ToString(VideoGame videogame)
+        {
+            return videogame.name;
+        }
+
+        public int CompareTo(VideoGame other)
+        {
+            if (other == null) return 1; // Current instance is greater than a null reference.
+
+            return string.Compare(this.name, other.name, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static void PublisherStatistics(List<VideoGame> list, string publisher)
+        {
+            int total = list.Count;
+            int publisherGameCount = list.Count(game => game.publisher == publisher);
+
+            double percentage = ((double)publisherGameCount / total) * 100;
+            percentage = (double)System.Math.Round(percentage, 2);
+            Console.WriteLine("Out of " + total + " games, " + publisherGameCount + " are made by " + publisher + ", which is " + percentage + "%.");
+        }
+
+        public static void GenreStatistics(List<VideoGame> list, string genre)
+        {
+            int total = list.Count;
+            int genreGameCount = list.Count(game => game.genre == genre);
+            double percentage = ((double)genreGameCount / total) * 100;
+            //rounding
+            percentage = (double)System.Math.Round(percentage, 2);
+            Console.WriteLine("Out of " + total + " games, " + genreGameCount + " are of the genre " + genre + ", which is " + percentage + "%.");
+        }
+
+        public static void PublisherData(List<VideoGame> list, string publisher)
+        {
+            
+            var pubGames = list.Where(videogame => videogame.publisher.Contains(publisher)).Select(videogame => videogame.name).ToList();
+            pubGames.Sort();
+            foreach (var name in pubGames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+        public static void GenreData(List<VideoGame> list, string genre)
+        {
+            var genreGames = list.Where(videogame => videogame.genre.Contains(genre)).Select(videogame => videogame.name).ToList();
+            genreGames.Sort();
+            foreach (var name in genreGames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
